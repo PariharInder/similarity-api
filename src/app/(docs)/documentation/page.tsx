4e0fe204cca555595479ac47851decbd214a2633
PariharInder <2002inderjitsingh@gmail.com>
2023-06-09 14:19:34 -0400

    added documentation page

---
+++ src/app/(docs)/documentation/page.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react'
+import LargeHeading from '@/ui/LargeHeading'
+import Paragraph from '@/ui/Paragraph'
+import DocumentationTabs from '@/components/DocumentTabs'
+
+interface pageProps {
+  
+}
+
+const page: FC<pageProps> = ({}) => {
+  return (
+    <div className='container max-w-7xl mx-auto mt-12'>
+    <div className='flex flex-col items-center gap-6'>
+      <LargeHeading>Making a request</LargeHeading>
+      <Paragraph>api/v1/similarity</Paragraph>
+
+      <DocumentationTabs />
+    </div>
+  </div>
+  )
+}
+
+export default page
